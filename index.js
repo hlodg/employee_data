667999dccea536793feedaa9c633efeb18e0d545
Hannah Lodge <lodg0021@umn.edu>
2022-04-25 12:35:00 -0500

    worked on creating role adding file, almost there

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const package=require('dotenv').config();
 const inquirer = require('inquirer');
 
 // const queryFunctions = require('./queryFunctions')
-const { allDepts} = require('./queryFunctions');
-const { allRoles } = require('./queryFunctions');
+const { allDepts, allRoles, allEmployees, addRole, updateEmployee, addEmployee} = require('./queryFunctions');
+// const { allRoles } = require('./queryFunctions');
 // Connect to database
 
-const options = ["ALL_DEPT", "ALL_ROLES"];
+const options = ["ALL_DEPT", "ALL_ROLES", "ALL EMPLOYEES", "ADD_ROLE", "ADD_EMPLOYEE", "UPDATE_EMPLOYEE_ROLE"];
 function startApp() {
     inquirer.prompt([
         {
@@ -22,8 +22,34 @@ function startApp() {
                 case options[0]:
                     allDepts();
                     break;
+
                 case options[1]:
                     allRoles();
+                    break;
+
+                case options[2]:
+                    allEmployees();
+                    break;
+                
+                case options[3]:
+                    inquirer
+                        .prompt([
+                            {
+                                type: "input",
+                                name: "role",
+                                message: "What role do you want to add?"
+                            }
+                        ]).then((ans)=>{
+                            addRole(ans)
+                        })
+                    break;
+                
+                case options[4]:
+                    addEmployee();
+                    break;
+                
+                case options[5]:
+                    updateEmployee();
                     break;
 
                 default:
--- queryFunctions.js
+++ queryFunctions.js
@@ -9,12 +9,14 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the database.`)
 );
+const cTable = require('console.table');
 
 function allDepts() {
     db.connect(function(err){
         if(err) throw err;
         db.query('SELECT * FROM department', function(err, result){
-            console.log(result);
+            console.table(result);
+            // create formatted table showing data
             if(err) throw err;
         });
     });
@@ -24,13 +26,45 @@ function allRoles(){
     db.connect(function(err){
         if(err) throw err;
         db.query('SELECT * FROM employees', function(err,result){
-            console.log(result);
+            console.table(result);
+            // create formatted table showing data
             if(err) throw err;
         })
     })
 }
 
+function allEmployees(){
+    db.connect(function(err){
+        if(err) throw err;
+        // fix select so that it is the right data
+        db.query('SELECT * FROM employees', function(err,result){
+            console.table(result);
+            // create formatted table showing data
+            if(err) throw err;
+        })
+    })
+}
+
+function addRole(ans){
+    db.query('INSERT INTO employees (role_name) VALUES (ans)',function(err,result){
+        console.log("We added the role!");
+        console.log(ans);
+    })
+}
+
+function addEmployee(){
+
+}
+
+function updateEmployee(){
+
+}
+
 module.exports = {
     allDepts,
-    allRoles
+    allRoles, 
+    allEmployees,
+    addEmployee,
+    updateEmployee,
+    addRole
 }
