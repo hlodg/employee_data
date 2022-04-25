f263a7c8721c0bb788aaeb8f2fe58b47fdcf4116
Hannah Lodge <lodg0021@umn.edu>
2022-04-25 10:28:54 -0500

    working on making inquirer work

---
+++ server.js
@@ -0,0 +1,51 @@
+// const mysql = require('mysql2');
+const inquirer = require('inquirer');
+require('dotenv').config();
+// const queryFunctions = require('./queryFunctions')
+const { allDepts } = require('./queryFunctions')
+// Connect to database
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        password: 'root',
+        database: 'employee_db'
+    },
+    console.log(`Connected to the database.`)
+);
+const options = ["ALL_DEPT", "ALL_ROLES"];
+function startApp() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "userview",
+            message: "What you want to see?",
+            choices: opt
+        }
+    ])
+    //     .then((ans) => {
+    //         // console.log(ans);
+    //         switch (ans.userview) {
+    //             case opt[0]:
+    //                 // queryFunctions.allDepts();
+    //                 allDepts();
+    //                 break;
+
+    //             default:
+    //                 break;
+    //         }
+    //     })
+}
+
+startApp();
+
+// function allDepts() {
+//     db.query('SELECT * FROM department', function (err, results) {
+//         console.log(results);
+//         startApp();
+//     });
+// }
+
+
